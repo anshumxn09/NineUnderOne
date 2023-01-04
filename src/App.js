970ed9f010f0a2f1450701c25cb763946829d879
Anshuman <sharmaaanshumaan@gmail.com>
2023-01-05 00:50:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <h1 data-aos="zoom-in">9 IN ONE</h1>
+      <div className="card">
+        {data.map((elem, index) => {
+          return (
+            <a href={elem.link} key={index} data-aos="zoom-in-up" title="CLICK TO VISIT APPLICATION">
+              <p>{elem.icon}</p>
+              <span>{elem.title}</span>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,50 @@
+
+const data = [
+    {
+      title : "GUESS THE NUMBER",
+      icon : <i class="fa-solid fa-plus-minus" />,
+      link : "https://anshumxn09.github.io/GuessTheNumber/"
+    }, 
+    {
+      title : "TIC TAC TOE",
+      icon : <i class="fa-solid fa-xmarks-lines"></i>,
+      link : "https://anshumxn09.github.io/TicTacToe.github.io/"
+    },
+    {
+      title : "MUSIC ALBUM",
+      icon : <i class="fa-solid fa-play"></i>,
+      link : " https://anshumxn09.github.io/MyDearMelancholy.github.io/"
+    },
+    {
+      title : "QUIZ APP",
+      icon : <i class="fa-solid fa-ranking-star"></i>,
+      link : "https://anshumxn09.github.io/QuizApp.github.io/"
+    },
+    {
+      title : "TWEET QUOTES",
+      icon : <i class="fa-solid fa-hashtag"></i>,
+      link : "https://anshumxn09.github.io/dynamicQuotes.github.io/"
+    },
+    {
+      title : "TO-DO",
+      icon : <i class="fa-solid fa-list"></i>,
+      link : "https://setgoalstowin.netlify.app/",
+    },
+    {
+      title : "TEXT TO SPEECH",
+      icon : <i class="fa-solid fa-volume-high"></i>,
+      link : "https://text2speeech.netlify.app/"
+    },
+    {
+      title : "FIND PALINDROME",
+      icon : <i class="fa-solid fa-magnifying-glass"></i>,
+      link : "https://findpalindrome.netlify.app/"
+    },
+    {
+      title : "CLICK TO KNOW",
+      icon : <i class="fa-solid fa-wand-magic-sparkles"></i>,
+      link : "https://anshumxnportfolio.netlify.app/"
+    },
+  ]
+
+export default data;
